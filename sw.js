983f2042f4805266caf50f99b43e1dfc99e51357
Grayson Hicks <graysonhicks@gmail.com>
2018-04-14 16:03:07 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-094caf2000a10b284fca.js","7dedc1f502e9a63086de39075619496e"],["/app-f7d343600b9d77034fb4.js","6f7b4f1ffc41c07f751ad5779d40396e"],["/commons-1f6cade73efba712a4cc.js","37cd4c6b117f7fb8aca289c34e74ef3a"],["/index.html","08c80d37a390c29e23bfcc9a0d044bb2"],["/manifest.json","879487c27e4ca1a733b915f512855a9f"],["/offline-plugin-app-shell-fallback/index.html","760578a58f36064119a1c7b4d37508f5"]];
+var precacheConfig = [["/react-scrolltop-button/app-544119e09d3b64a5e5e6.js","07b873dabec3b42c9fcf19a1531a3e02"],["/react-scrolltop-button/commons-1f6cade73efba712a4cc.js","b3076d0e80f162b2f2a8aedcc7ae1378"],["/react-scrolltop-button/index.html","0a7bafb65f907671c916e2ef8fd82d0a"],["/react-scrolltop-button/manifest.json","879487c27e4ca1a733b915f512855a9f"],["/react-scrolltop-button/offline-plugin-app-shell-fallback/index.html","43f884d305aed70504c371399d1216de"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
